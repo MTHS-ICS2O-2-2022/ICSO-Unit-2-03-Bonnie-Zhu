7963e8710ce2f83599d2de92f38eacf3f2f5d995
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-03-20 17:26:54 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie Zhu
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("Name").value
+  const userAge = parseInt(document.getElementById("Age").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your info is: " + firstName + ", age " + userAge + "."
+}
